54a9b2cb070a8441e9fb7dd5d6444671cb8a4957
Chris Burnham <chris@burnhamup.com>
2026-02-08 14:07:39 -0800

    updating the base url

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,14 @@
-import './App.css'
+import "./App.css";
 
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import MapPage from './pages/MapPage';
-import { GW2ApiAccountProvider } from './context/apiContext';
-import Page from './pages/Page';
-
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import MapPage from "./pages/MapPage";
+import { GW2ApiAccountProvider } from "./context/apiContext";
+import Page from "./pages/Page";
 
 function App() {
   return (
     <GW2ApiAccountProvider>
-      <Router>
+      <Router basename={import.meta.env.BASE_URL}>
         <Routes>
           <Route path="/map/:name" element={<MapPage />} />
           <Route path="/" element={<Page />} />
@@ -19,4 +18,4 @@ function App() {
   );
 }
 
-export default App
+export default App;
