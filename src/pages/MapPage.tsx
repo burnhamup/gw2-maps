077b5838c672a67578118acd932aa168c5ebb219
Chris Burnham <chris@burnhamup.com>
2026-02-08 14:12:21 -0800

    updating the config file fetching

--- src/pages/MapPage.tsx
+++ src/pages/MapPage.tsx
@@ -1,28 +1,29 @@
-
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { MapData } from "../components/Achievement";
 import { Map } from "../components/Map";
 
 export default function MapPage() {
-    const { name } = useParams();
-    const [ mapData, setMapData] = useState<MapData>({name: "Loading...", achievements: []});
+  const { name } = useParams();
+  const [mapData, setMapData] = useState<MapData>({
+    name: "Loading...",
+    achievements: [],
+  });
 
-    useEffect(() => {
-        const fetchAchievementData = async() => {
-            try {
-                const mapRequest = await fetch(`/config/${name}.json`);
-                const mapJson = await mapRequest.json();
-                setMapData(mapJson);
-            } catch (err) {
-                // I guess 404?
-            }
-        };
-        
-        fetchAchievementData();
-    }, [name]);
-    return (<Map
-        mapData={mapData}
-    />);
+  useEffect(() => {
+    const fetchAchievementData = async () => {
+      try {
+        const mapRequest = await fetch(
+          `${import.meta.env.BASE_URL}/config/${name}.json`,
+        );
+        const mapJson = await mapRequest.json();
+        setMapData(mapJson);
+      } catch (err) {
+        // I guess 404?
+      }
+    };
 
-};
+    fetchAchievementData();
+  }, [name]);
+  return <Map mapData={mapData} />;
+}
